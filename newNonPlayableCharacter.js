70fa1acf9b35b48bd0d9bce1951f945ac95415d6
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-11 14:06:06 -0400

    moveCharacter working

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,10 +1,10 @@
-function newNonPlayableCharacter(x, y) {
+const newNonPlayableCharacter = (x, y) => {
     let element = newImage('assets/red-character/static.gif')
     element.style.zIndex = 1;
     
     let direction = null;
 
-    function moveCharacter() {
+    const moveCharacter = () => {
         if (direction === 'west') {
             x -= 1
         }
@@ -23,27 +23,35 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    const walkEast = async (time) => {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time);
+        stop();
     }
 
-    function walkNorth() {
+    const walkNorth = async (time) => {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time);
+        stop();
     }
 
-    function walkWest() {
+    const walkWest = async (time) => {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time);
+        stop();
     }
 
-    function walkSouth() {
+    const walkSouth = async (time) => {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time);
+        stop();
     }
 
-    function stop() {
+    const stop = () => {
         direction = null
         element.src = `./assets/red-character/static.gif`
     }
@@ -56,4 +64,10 @@ function newNonPlayableCharacter(x, y) {
         walkSouth: walkSouth,
         stop: stop
     }
+}
+
+const sleep = (time) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
 }
